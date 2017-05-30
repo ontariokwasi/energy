6ffe0f470ed0bebbddb416d58fb368cc8503934c
ontariokwasi <ontariokwasi@gmail.com>
2017-05-30 02:45:32 +0000

    Add files via upload

---
+++ www/js/my-app.js
@@ -0,0 +1,49 @@
+// Initialize your app
+var myApp = new Framework7({
+    animateNavBackIcon:true
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add main View
+var mainView = myApp.addView('.view-main', {
+    // Enable dynamic Navbar
+    dynamicNavbar: true,
+    // Enable Dom Cache so we can use all inline pages
+    domCache: true
+});
+
+$$('.notification-default').on('click', function () {
+    myApp.addNotification({
+        title: 'Framework7',
+        message: 'This is a simple notification message with title and message'
+    });
+});
+$$('.notification-full').on('click', function () {
+    myApp.addNotification({
+        title: 'Framework7',
+        subtitle: 'Notification subtitle',
+        message: 'This is a simple notification message with custom icon and subtitle',
+        media: '<i class="icon icon-f7"></i>'
+    });
+});
+$$('.notification-custom').on('click', function () {
+    myApp.addNotification({
+        title: 'My Awesome App',
+        subtitle: 'New message from John Doe',
+        message: 'Hello, how are you? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut posuere erat. Pellentesque id elementum urna, a aliquam ante. Donec vitae volutpat orci. Aliquam sed molestie risus, quis tincidunt dui.',
+        media: '<img width="44" height="44" style="border-radius:100%" src="http://lorempixel.com/output/people-q-c-100-100-9.jpg">'
+    });
+});
+$$('#notification-callback').on('click', function () {
+    myApp.addNotification({
+        title: 'My Awesome App',
+        subtitle: 'New message from John Doe',
+        message: 'Hello, how are you? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut posuere erat. Pellentesque id elementum urna, a aliquam ante. Donec vitae volutpat orci. Aliquam sed molestie risus, quis tincidunt dui.',
+        media: '<img width="44" height="44" style="border-radius:100%" src="http://lorempixel.com/output/people-q-c-100-100-9.jpg">',
+        onClose: function () {
+            myApp.alert('Notification closed');
+        }
+    });
+});
